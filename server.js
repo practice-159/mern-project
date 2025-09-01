be1bce335276405b2ddc6101bde9402011f0644a
practice058 <96.peddler-coop@icloud.com>
2025-09-01 21:54:07 +0800

    Make changes for deplayment

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const authRoute = require("./routes").auth;
+const courseRoute = require("./routes").course;
+const passport = require("passport");
+require("./config/passport")(passport);
+const cors = require("cors");
+const path = require("path");
+const port = process.env.PORT || 8080;
+
+// 連結MongoDB
+mongoose
+  .connect(process.env.MONGODB_CONNECTION)
+  .then(() => {
+    console.log("Connecting to mongodb...");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.use("/api/user", authRoute);
+
+// course route應該被jwt保護
+// 如果request header內部沒有jwt，則request就會被視為是unauthorized
+app.use(
+  "/api/courses",
+  passport.authenticate("jwt", { session: false }),
+  courseRoute
+);
+
+// 只有登入系統的人，才能夠去新增課程或是註冊課程
+// jwt
+
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+app.listen(port, () => {
+  console.log("後端伺服器聆聽在port 8080...");
+});
