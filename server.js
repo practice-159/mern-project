19d9fa119124c535e5310e107a2d0125fcf6d030
practice058 <96.peddler-coop@icloud.com>
2025-09-01 22:06:30 +0800

    Fix

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ if (
   process.env.NODE_ENV === "production" ||
   process.env.NODE_ENV === "staging"
 ) {
-  app.get("*", (req, res) => {
+  app.get("/(.*)/", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
