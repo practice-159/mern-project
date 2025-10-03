ff18b04dfe6ddd3ab63d6c9765aff8c288f4a6c4
practice-159 <96.peddler-coop@icloud.com>
2025-10-03 21:26:24 +0800

    Update home-component.js

--- client/src/components/home-component.js
+++ client/src/components/home-component.js
@@ -44,7 +44,7 @@ const HomeComponent = () => {
         </div>
 
         <footer className="pt-3 mt-4 text-muted border-top">
-          &copy; 2023 Wilson Ren
+          
         </footer>
       </div>
     </main>
